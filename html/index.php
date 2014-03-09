a94d32673b535138edbc4e71613bdd7f164f6587
Niyas C <niyasc@localhost.localdomain>
2014-03-09 14:46:07 +0530

    Initial commit, may be final too, work almost completed, integration with facebook remains

---
+++ html/index.php
@@ -0,0 +1,4 @@
+<?php
+	require_once("../includes/config.php");
+	render('test-home.php',["title"=>"Personality Test"]);
+?>
---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<head>
+<?php if (isset($title)): ?>
+            <title>Personality Test : <?= htmlspecialchars($title) ?></title>
+        <?php else: ?>
+            <title>Personality Test</title>
+        <?php endif ?>
+<link rel="stylesheet" type="text/css" href="./css/mystyle.css">
+</head>
+<div class='header'>
+Personality Test
+</div>
